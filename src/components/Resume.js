70b60d7cbc0ca09d59583acfae6f92fd0094302b
Meredith Fontana <meredith.fontana@gmail.com>
2019-04-27 17:41:54 -0600

    fixed siding of header

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,31 +8,40 @@ class Resume extends Component {
             <div>
             
             <Timeline lineColor={'#E2E1E3'}>
+  
+  
+
+ <TimelineItem
+    key="001"
+    dateText="8/2012 – 8/2012"
+    dateInnerStyle={{ background: '#F0DB5B', color: '#000' }}
+  >
+    <h2 style={{ color: '#8ED649' }}>Yoga Instructor</h2>
+    <h4>Core Power Yoga</h4>
+    <h5 style={{color: 'grey'}}>Denver, CO</h5>
+        <p>
+        Consistently receive highest ratings on performance evaluations conducted by studio managers. 
+        </p>
+        <p>
+        Promoted 3 times throughout my employment.
+        </p>
+        <p>
+        Currently teach at the highest level of instruction and selected to be a member of the teacher training instructional staff.
+        </p>
+  </TimelineItem>
+            
+  
   <TimelineItem
     key="001"
     dateText="8/2012 – 8/2012"
     dateInnerStyle={{ background: '#F0DB5B', color: '#000' }}
   >
-    <h3 style={{ color: '#8ED649' }}>Student Research Associate</h3>
+    <h2 style={{ color: '#8ED649' }}>Student Research Associate</h2>
     <h4>Smithsonian National Museum of Natural History</h4>
         <p>
         Conducted paleontological research on Cretaceous polyglyphanodontian lizards
         alongside NMNH Senior Scientists in the Department of Paleobiology.
         </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
   </TimelineItem>
 
     <TimelineItem
@@ -40,7 +49,7 @@ class Resume extends Component {
     dateText="8/2012 – 8/2012"
     dateInnerStyle={{ background: '#F0DB5B', color: '#000' }}
     >
-    <h3 style={{ color: '#8ED649' }}>Master of Science Degree in Evolutionary Biology</h3>
+    <h2 style={{ color: '#8ED649' }}>Master of Science Degree in Evolutionary Biology</h2>
     <h4>The George Washginton University</h4>
 
     </TimelineItem>
@@ -51,7 +60,7 @@ class Resume extends Component {
     dateText="8/2012 – 8/2012"
     dateInnerStyle={{ background: '#F0DB5B', color: '#000' }}
     >
-    <h3 style={{ color: '#8ED649' }}>Master of Science Degree in Evolutionary Biology</h3>
+    <h2 style={{ color: '#8ED649' }}>Master of Science Degree in Evolutionary Biology</h2>
     <h4>The George Washginton University</h4>
         <p>
         Conducted paleontological research on Cretaceous polyglyphanodontian lizards
@@ -90,23 +99,11 @@ class Resume extends Component {
       boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
     }}
   >
-    <h3 style={{ color: 'white' }}>Master of Science Degree in Evolutionary Biology</h3>
+    <h2 style={{ color: 'white' }}>Master of Science Degree in Evolutionary Biology</h2>
     <h4 style={{ color: '#61b8ff' }}>The George Washington University</h4>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
   </TimelineItem>
+
+
   <TimelineItem
     key="003"
     dateComponent={(
@@ -123,7 +120,7 @@ class Resume extends Component {
       </div>
     )}
   >
-    <h3>Title, Company</h3>
+    <h2>Title, Company</h2>
     <h4>Subtitle</h4>
     <p>
       Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
@@ -152,7 +149,7 @@ class Resume extends Component {
     dateText="08/2008 – 11/2008"
     dateInnerStyle={{ background: '#76bb7f' }}
   >
-    <h3>Title, Company</h3>
+    <h2>Title, Company</h2>
     <h4>Subtitle</h4>
     <p>
       Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
